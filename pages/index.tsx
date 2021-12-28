f9c896e3080e4a3e8d7fba6372220730ba8b6dd9
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-28 11:42:28 -0300

    Finalizando Criação de Portas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
+import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
+  /*const [p1, setP1] = useState(new PortaModel(1))
+  const [p2, setP2] = useState(new PortaModel(2))
+  const [p3, setP3] = useState(new PortaModel(3))*/
+
+  const [portas, setPortas] = useState(criarPortas(3,2));
  
+
+  function renderizarPortas(){
+    return portas.map((porta) => {
+      return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta)) } />
+    })
+  }
+
+
   return (
-    <div style={{ display: "flex", flexDirection: "column"}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+    <div style={{ display: "flex" }}>
+      {renderizarPortas()}
     </div>
   );
 }
