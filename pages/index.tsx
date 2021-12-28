9b5076c811c84c12abf6c26a2a5ce3f5046e30aa
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-28 11:49:46 -0300

    Exibindo Porta com Presente

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,7 @@ import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
-  /*const [p1, setP1] = useState(new PortaModel(1))
-  const [p2, setP2] = useState(new PortaModel(2))
-  const [p3, setP3] = useState(new PortaModel(3))*/
-
-  const [portas, setPortas] = useState(criarPortas(3,2));
+  const [portas, setPortas] = useState(criarPortas(4,1));
  
 
   function renderizarPortas(){
